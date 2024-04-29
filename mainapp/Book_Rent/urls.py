e726329df06a57abb0c7891ecd3b9d90f5fb24af
vcube-8 <projectvcube8@gmail.com>
2024-04-29 20:40:58 +0530

    modified static files

--- mainapp/Book_Rent/urls.py
+++ mainapp/Book_Rent/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.homefun,name="homeurl"),
+    path('shop/', views.shopfun,name="shopurl"),
     path('sell/',views.sellfun,name="sellurl"),
     path('about/',views.aboutfun,name="abouturl"),
     path('contact/',views.contactfun,name="contacturl"),
     path('services/',views.servicesfun,name="servicesurl"),
-
 ]
--- mainapp/Book_Rent/views.py
+++ mainapp/Book_Rent/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def homefun(request):
+def shopfun(request):
     return render(request,'Base.html')
 
 def sellfun(request):
