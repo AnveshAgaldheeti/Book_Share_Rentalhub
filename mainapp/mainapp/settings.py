eb138e2da1c6315110946614da820266df998390
LillyFlorence46 <projectvcube8@gmail.com>
2024-04-29 15:33:55 +0530

    database added

--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Book_Share_Rentalhub',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':3306,
     }
 }
 
