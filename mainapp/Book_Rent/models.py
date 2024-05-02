44309e9ef9b2ac4ed283679cb3d34e324ec01acb
AnveshAgaldheeti <anveshagaldheeti@gmail.com>
2024-05-02 20:18:45 +0530

    added category model

--- mainapp/Book_Rent/models.py
+++ mainapp/Book_Rent/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
 class Sell_model(models.Model):
     title=models.CharField(max_length=50)
     author=models.CharField(max_length=50)
