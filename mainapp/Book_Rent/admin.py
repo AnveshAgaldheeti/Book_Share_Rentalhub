2ee858c0bd1fb09dc5ea1dfe633f1bab92c37d6a
AnveshAgaldheeti <anveshagaldheeti@gmail.com>
2024-05-02 20:53:29 +0530

    updated admin page

--- mainapp/Book_Rent/admin.py
+++ mainapp/Book_Rent/admin.py
@@ -5,5 +5,7 @@ from .models import Sell_model,Category
 admin.site.register(Category)
 
 class sell_admin(admin.ModelAdmin):
-    list_display='__all__'
-admin.site.register(Sell_model,)
+    list_display=['title','author','price','uploaded_by','categories']
+    list_filter=['categories','price']
+    search_fields=['title','author','price']
+admin.site.register(Sell_model,sell_admin)
