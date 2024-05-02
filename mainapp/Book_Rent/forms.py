bdf03463b3a4935fc0f2d7f7ce7d1a103df5e54f
AnveshAgaldheeti <anveshagaldheeti@gmail.com>
2024-05-02 19:53:32 +0530

    modefied form added place holder in signup form

--- mainapp/Book_Rent/forms.py
+++ mainapp/Book_Rent/forms.py
@@ -12,6 +12,15 @@ class Sell_form(forms.ModelForm):
 
 class signupform(UserCreationForm):
     email=forms.EmailField()
+    def __init__(self, *args, **kwargs):
+        super(signupform, self).__init__(*args, **kwargs)
+
+        # Set placeholders for form fields
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+
     class Meta:
         model=User
         fields=['username','email','password1','password2']
